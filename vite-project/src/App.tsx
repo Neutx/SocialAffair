f0369396af563f104792b3760139a64238eda295
Adil Parwez <dlprwz@gmail.com>
2024-09-12 03:00:16 +0530

    shadcn initialized

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,6 +4,8 @@ import './globals.css';
 import SigninForm from './_auth/form/SigninForm';
 import SignupForm from './_auth/form/SignupForm';
 import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 
 const App = () => {
@@ -11,12 +13,23 @@ const App = () => {
     <main className='flex h-screen'>
         <Routes>
             {/*public route*/}
+            <Route element={<AuthLayout/>}>
+
             <Route path="sign-in" element={<SigninForm/>} />
             <Route path="sign-up" element={<SignupForm/>} />
+            
+            </Route>
+
 
 
             {/*private route*/}
+
+            <Route element={<RootLayout/>}>
+
             <Route index element={<Home/>} />
+
+            </Route>
+
         </Routes>
 
     </main>
